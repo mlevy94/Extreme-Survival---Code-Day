7aa93c239244d288d0cd8ba37efe271f642367b7
MasterCzar <sarthak.wahal@gmail.com>
2015-05-23 23:31:20 -0500

    event probabilities

--- server/event.py
+++ server/event.py
@@ -4,7 +4,7 @@ __author__ = 'Wes'
 
 COMPUTERS = ['computer', 'laptop', 'desktop']
 GET = ['get', 'grab']
-NOTHING = ['sit', 'do nothing', 'no action', 'ignore']
+NOTHING = ['sit', 'do nothing', 'no action', 'ignore', 'nothing']
 WORK = ['work', 'code', 'design']
 YES = ['yes', 'accept', 'y']
 NO = ['no', 'reject', 'n']
@@ -12,6 +12,9 @@ REST = ['nap', 'rest', 'sleep']
 DRINK = ['drink']
 ENERGY_DRINK = ['energy', 'redbull', 'red bull', 'monster', 'coffee']
 SEARCH = ['search', 'download', 'google', 'online', 'find API']
+GOOD =  ['q','z','m','p','0','2']
+BAD = !GOOD
+INVESTIGATE = ['check', 'investigate', 'find']
 
 class Option:
 
@@ -106,24 +109,6 @@ class TiredEvent(Event):
             # raise bladder probability and increrase health problems.
         super().run(game, player)
 
-class TiredEvent(Event):
-    def __init__(self):
-        super().__init__("Feeling Tired", "", [Option([REST], "Rest"), Option([NOTHING], "Ignore"), Option([DRINK, ENERGY_DRINK], "Have Energy Drink")], 5)
-
-    def run(self, game, player):
-        if self.option_picked == 0:
-            self.end_display = """You are no longer tired. The sleep cost you 1 hour of coding time"""
-            self.dtime = -60
-        elif self.option_picked == 1:
-            self.end_display = """You choose to work on in the tired state.
-            This has its consequences"""
-            self.dtime = 0
-            # Redirect to next option soon
-        else:
-            self.end_display = "You are fully charged. Be careful not to overdo the drinks"
-            self.dtime = 30
-            # raise bladder probability and increrase health problems.
-        super().run(game, player)
 
 class NewCodeNeeded(Event):
     def __init__(self):
@@ -131,23 +116,24 @@ class NewCodeNeeded(Event):
 
     def run(self, game, player):
         if self.option_picked == 0:
-            #random option
-            self.end_display = """The Code Works. You save half an hour"""
-            self.dtime = 30
-            #not working
-            self.end_display = """There are bugs. They took time to fix
-            You lost an Hour"""
-            self.dtime = -60
+            if random.uniform(0, 1000) < 700:
+                self.end_display = """The Code Works. You save half an hour"""
+                self.dtime = 30
+            else:
+                self.end_display = """There are bugs. They took time to fix
+                You lost an Hour"""
+                self.dtime = -60
         elif self.option_picked == 1:
             self.end_display = """Work grings to a halt for two hours due to lack of API's"""
             self.dtime = -120
             # Redirect to next option soon
         else:
-            #Random Option
-            self.end_display = "You found useful code online. It saves you 2 hours"
-            self.dtime = 120
-            # small probability
-            self.end_display = "File filled with viruses. All data Corrupted. Must start over."
+            if random.uniform(0, 1000) < 960:
+                self.end_display = "You found useful code online. It saves you 2 hours"
+                self.dtime = 120
+            else:
+                self.end_display = "File filled with viruses. All data Corrupted. Must start over."
+                #end game
         super().run(game, player)
 
 class NewMember(Event):
@@ -156,17 +142,66 @@ class NewMember(Event):
 
     def run(self, game, player):
         if self.option_picked == 0:
-            #random option
-            self.end_display = """The new member is a genius. He saves two hours"""
-            self.dtime = 120
-            #2
-            self.end_display = """The new guy is good. One hour is saved"""
-            self.dtime = 60
-            #3
-            self.end_display = """The new guy is distracting. One hour lost"""
-            self.dtime = -60
+            if random.uniform(0, 1000) < 300:
+                self.end_display = """The new member is a genius. He saves two hours"""
+                self.dtime = 120
+            else if random.uniform(0, 1000) < 700:
+                self.end_display = """The new guy is good. One hour is saved"""
+                self.dtime = 60
+            else:
+                self.end_display = """The new guy is distracting. One hour lost"""
+                self.dtime = -60
         else:
-            self.end_display = "He joins another team. You continued with your work." \
-                               ""
+            self.end_display = "He joins another team. You continued with your work."
             self.dtime = 0
+        super().run(game, player)
+
+class ForgotPassword(Event):
+    def __init__(self):
+        super().__init__("""As you rouse your computer from its deep slumber, it presents you with a familiar screen:
+            a red background, with some text informing you that your device is locked. You move your cursor over the familiar dialogue box, and…""", "", [Option([GOOD], "Success"), Option([BAD], "Failure")], 5)
+
+    def run(self, game, player):
+        if self.option_picked == 0:
+            self.end_display = """You log in...Moving on to more important things"""
+            #break
+        else:
+            self.end_display = """Your computer thinks for a moment then responds with a large “Password Incorrect” message.
+            Huh? That’s odd, you know your own password, and you’ve used the same one since high school…
+            You should probably have changed it since then but you know, I’m sorry, knew your password.
+            Well not much left to do but start guessing."""
+            self.dtime = -15
+            #repeat request for password
+        super().run(game, player)
+
+class Shaking(Event):
+    def __init__(self):
+        super().__init__("""You feel a slight tremor""", "", [Option([NOTHING], "Ignore"), Option([INVESTIGATE], "Investigate"), Option([HIDE], "Hide")], 5)
+
+    def run(self, game, player):
+        if self.option_picked == 0:
+            if random.uniform(1,1000)< 400:
+                def __init__(self):
+                    super().__init__("""A Pterodactyl breaks in through the roof""", "", [Option([Nothing], "Ignore"), Option([HIDE], "Hide")], 5)
+            else if random.uniform(1,1000) < 400:
+                self.end_display = """An earthquake causes the roof to fall. Lose 1 hour finding another location to code"""
+                self.dtime = -60
+            else:
+                self.end_display = """It was the constuction work, continue to code."""
+        else if self.option_picked == 1;
+            if random.uniform(1,1000)< 400:
+                elf.end_display = #insert Scenario
+                self.dtime = -30
+            else:
+                elf.end_display = #insert scenario
+        else:
+            if random.uniform(1,1000)< 400:
+                elf.end_display = """Wasted half hour hiding from the sound of the construction work.
+                Assured of safety, you continue work"""
+                self.dtime = -30
+            else:
+                elf.end_display = """Your preparedness for the earthquake saved you valuable time. Continue to code"""
+
+
+
         super().run(game, player)
